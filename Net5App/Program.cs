c292f204a74cdc1ec3008f2f9db0e507429168d8
A_And <A_And@DESKTOP-8720PJ0>
2022-10-26 06:31:11 -0500

    Add project files.

---
+++ Net5App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.Tracing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Net5App
+{
+    internal class Program
+    {
+        // constants/immutables are fields that are assigned outside of the method but inside of the class     
+        
+
+        static void Main(string[] args)
+        {
+
+           
+            
+        }
+        
+     }                      
+    } 
+    
+
